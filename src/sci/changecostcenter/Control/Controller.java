b340284bb596a4c0243bd647cf13d371e705a7f6
Luan Grabher <lujetix@hotmail.com>
2020-05-25 08:59:23 -0300

    Added get contability entries in controller

--- src/sci/changecostcenter/Control/Controller.java
+++ src/sci/changecostcenter/Control/Controller.java
@@ -2,10 +2,18 @@ package sci.changecostcenter.Control;
 
 import Entity.Executavel;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import sci.changecostcenter.Model.CostCenterModel;
+import sci.changecostcenter.Model.Entity.ContabilityEntry;
 import sql.Database;
 
 public class Controller {
-    public class defineDatabase extends Executavel{
+
+    private static String reference = "202001";
+    private static List<ContabilityEntry> contabilityEntries = new ArrayList<>();
+
+    public class defineDatabase extends Executavel {
 
         public defineDatabase() {
             nome = "Definindo banco de dados...";
@@ -14,6 +22,18 @@ public class Controller {
         @Override
         public void run() {
             Database.setStaticObject(new Database(new File("sci.cfg")));
-        }        
+        }
+    }
+
+    public class getContabilityEntries extends Executavel {
+
+        public getContabilityEntries() {
+            nome = "Buscando lançamentos contábeis sem centro de custo";
+        }
+
+        @Override
+        public void run() {
+            contabilityEntries = CostCenterModel.getContabilityEntriesWithoutCostCenter(reference);
+        }
     }
 }
