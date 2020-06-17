5606a6bd2a94003022f9afe03afb1cd3384cf37e
Luan Grabher <lujetix@hotmail.com>
2020-06-17 09:59:43 -0300

    Added test function

--- test/testPack/testMain.java
+++ test/testPack/testMain.java
@@ -1,14 +1,23 @@
 package testPack;
 
+import java.io.File;
+import sci.changecostcenter.SCIChangeCostCenter;
+
 public class testMain {
 
 
     public static void main(String[] args) {
-         Integer val = null;
+         Integer month = 4;
+         Integer year  = 2020;
          
-         String ff = val + "";
+         String name = "Teste";
          
-         System.out.println(ff);
+         File folder = new File("G:\\Contábil\\Clientes\\Zampieron & Dalallacorte\\Escrituração Mensal\\2020\\Movimento");
+         
+         File swapsFile = new File(folder.getAbsolutePath() + "\\");
+         File expensesFile = new File(folder.getAbsolutePath() + "\\");
+         
+         SCIChangeCostCenter.mainFunction(name, month, year, swapsFile, expensesFile);
     }
     
 }
