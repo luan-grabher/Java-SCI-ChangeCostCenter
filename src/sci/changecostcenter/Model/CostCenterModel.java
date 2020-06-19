601b379576004a27aaeb6bab9e10c90d955feedf
Luan Grabher <lujetix@hotmail.com>
2020-06-19 14:58:28 -0300

    added sql to select Reference Cost Centers on Database

--- src/sci/changecostcenter/Model/CostCenterModel.java
+++ src/sci/changecostcenter/Model/CostCenterModel.java
@@ -18,8 +18,24 @@ import sql.Database;
 public class CostCenterModel {
 
     private List<ContabilityEntry> contabilityEntries = new ArrayList<>();
+    private List<CostCenterEntry> costCenters = new ArrayList<>();
 
     private List<Swap> swaps;
+
+    public List<CostCenterEntry> getReferenceCostCenterEntries(String reference) {
+        //Reset
+        costCenters = new ArrayList<>();
+
+        Map<String, String> variables = new HashMap<>();
+        variables.put("enterpriseCode", Env.get("changeCostCenterEnterpriseCode"));
+        variables.put("reference", reference);
+
+        List<String[]> results = Database.getDatabase().select(new File("sql\\selectReferenceContabilityEntriesCostCenters.sql"), variables);
+        
+        
+        
+        return costCenters;
+    }
 
     public List<ContabilityEntry> getContabilityEntriesWithoutCostCenter(String reference) {
         //Reset
@@ -33,22 +49,22 @@ public class CostCenterModel {
         List<String[]> results = Database.getDatabase().select(new File("sql\\selectContabilityEntriesWithoutCostCenter.sql"), variables);
 
         Integer enterpriseCode = Integer.valueOf(Env.get("changeCostCenterEnterpriseCode"));
-        
+
         //PErcorre resultados
         for (String[] result : results) {
             ContabilityEntry entry = new ContabilityEntry();
             entry.setEnterpriseCode(enterpriseCode);
             entry.setKey(Integer.valueOf(result[1]));
             entry.setDefaultPlan(Integer.valueOf(result[2]));
-            entry.setAccountDebit(result[3]==null?null:Integer.valueOf(result[3]));
-            entry.setAccountCredit(result[4]==null?null:Integer.valueOf(result[4]));
+            entry.setAccountDebit(result[3] == null ? null : Integer.valueOf(result[3]));
+            entry.setAccountCredit(result[4] == null ? null : Integer.valueOf(result[4]));
             entry.setDate(Dates.getCalendarFromFormat(result[5], "yyyy-mm-dd"));
             entry.setValue(new BigDecimal(result[6]));
             entry.setDescriptionComplement(result[7]);
             entry.setEntryType(Integer.valueOf(result[8]));
             entry.setDocument(result[9]);
-            entry.setParticipantDebit(result[11]==null?null:Integer.valueOf(result[11]));
-            entry.setParticipantCredit(result[12]==null?null:Integer.valueOf(result[12]));
+            entry.setParticipantDebit(result[11] == null ? null : Integer.valueOf(result[11]));
+            entry.setParticipantCredit(result[12] == null ? null : Integer.valueOf(result[12]));
 
             contabilityEntries.add(entry);
             //if((result[4]==null?0:Integer.valueOf(result[4])) == 265){
@@ -74,7 +90,7 @@ public class CostCenterModel {
     public void importCostCenterEntriesToDatabase() {
         for (Swap swap : swaps) {
             for (CostCenterEntry entry : swap.getEntries()) {
-                if (entry.getKey() != 0) {
+                if (entry.getKey() != null && entry.getKey() != 0) {
                     insertContabilityEntryCostCenter(entry);
                 }
             }
@@ -106,16 +122,16 @@ public class CostCenterModel {
         //Cria variavel de trocas
         Map<String, String> variableChanges = new HashMap<>();
 
-        variableChanges.put("enterpriseCode", Env.get("enterpriseCode"));
-        variableChanges.put("centerCostPlan", Env.get("centerCostPlan"));
+        variableChanges.put("enterpriseCode", Env.get("changeCostCenterEnterpriseCode"));
+        variableChanges.put("centerCostPlan", Env.get("changeCostCenterCenterCostPlan"));
         variableChanges.put("key", entry.getKey().toString()); // Chave do lançamento        
         variableChanges.put("value", entry.getValue().toString());
         variableChanges.put("valueType", entry.getValueType().toString());
         variableChanges.put("centerCost", entry.getCostCenter().toString());
 
-        Database.getDatabase().query(scriptSqlInsertContabilityEntryCostCenter, variableChanges);
-        
-        System.out.println("Inserido centro de custo " + entry.getCostCenter() + " na chave " + entry.getKey());
+        boolean result = Database.getDatabase().query(scriptSqlInsertContabilityEntryCostCenter, variableChanges);
+
+        System.out.println("Inserido centro de custo " + entry.getCostCenter() + " na chave " + entry.getKey() + ": " + result);
     }
 
     private final String scriptSqlGetLastContabilityEntryKey = FileManager.getText(new File("sql\\selectLastContabilityEntryKey.sql"));
